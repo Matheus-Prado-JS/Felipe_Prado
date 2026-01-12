eb550660771fd48583d39c9ee1f03ffe459b0f57
Matheus-Prado-JS <pradomatheus937@gmail.com>
2026-01-12 14:29:33 -0300

    atualização 1.3.7

--- script.js
+++ script.js
@@ -217,6 +217,11 @@ function loadVideo(index) {
       <div class="progress-bar"><div class="progress"></div></div>
       <span class="time">0:00</span>
       <input type="range" class="volume" min="0" max="100" value="100" />
+      <button class="fullscreen-btn" aria-label="Tela cheia">
+      <svg viewBox="0 0 24 24" class="icon fullscreen">
+        <path d="M7 14H5v5h5v-2H7v-3zm0-4h2V7h3V5H7v5zm10 9h-3v2h5v-5h-2v3zm0-14V5h-5v2h3v3h2V5z"/>
+      </svg>
+    </button>
     </div>
   `;
 
@@ -313,6 +318,28 @@ function formatTime(sec) {
   const s = Math.floor(sec % 60).toString().padStart(2, "0");
   return `${m}:${s}`;
 }
+// ===============================  
+// Pausar vídeo clicando no vídeo
+// ===============================
+const customPlayer = document.querySelector(".custom-player");
+
+function togglePlay() {
+  if (!player) return;
+
+  const state = player.getPlayerState();
+
+  if (state === YT.PlayerState.PLAYING) {
+    player.pauseVideo();
+  } else {
+    player.playVideo();
+  }
+}
+
+// Desktop
+customPlayer.addEventListener("click", togglePlay);
+
+// Mobile / Safari
+customPlayer.addEventListener("touchstart", togglePlay);
 
 // ===============================
 // Abrir modal
